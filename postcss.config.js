bb4a7b79e57d52f6250ed6ea877109bf9649c815
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-01-13 01:11:38 -0800

    integrated purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,23 @@
 /* eslint-disable */
 
-module.exports = {
-  plugins: {
-    'postcss-preset-env': {},
-    autoprefixer: {},
-    cssnano: {},
-  },
+const postcsspresetenv = require('postcss-preset-env');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+module.exports = ctx => {
+  return {
+    plugins: [
+      postcsspresetenv({}),
+      autoprefixer({}),
+      ...(ctx.env === 'production'
+        ? [
+            purgecss({
+              content: ['./**/*.html'],
+            }),
+            cssnano({}),
+          ]
+        : []),
+    ],
+  };
 };
