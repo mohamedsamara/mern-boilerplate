29fd794e785df015813f94a89758f3053056e663
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-01-15 19:52:42 -0800

    implemented babel cache

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = api => {
+  api.cache.using(() => process.env.NODE_ENV);
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          useBuiltIns: 'usage',
+          corejs: '3.0.0',
+        },
+      ],
+      '@babel/preset-react',
+    ],
+  };
+};
