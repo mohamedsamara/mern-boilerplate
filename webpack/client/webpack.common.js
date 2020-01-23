cb80fe53692e8f6903d1be5a03b7f086be42ff7e
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-01-22 17:21:00 -0800

    reduced ant design bundle

--- webpack/client/webpack.common.js
+++ webpack/client/webpack.common.js
@@ -6,6 +6,7 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const NODE_ENV = process.env.NODE_ENV;
 const CURRENT_WORKING_DIR = process.cwd();
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
 
 module.exports = {
   entry: [path.join(CURRENT_WORKING_DIR, 'client/app/index.tsx')],
@@ -21,6 +22,10 @@ module.exports = {
       '.html',
     ],
     alias: {
+      '@ant-design/icons/lib/dist$': path.join(
+        CURRENT_WORKING_DIR,
+        'client/app/icons.tsx',
+      ),
       app: 'client/app',
     },
   },
@@ -50,6 +55,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new AntdDayjsWebpackPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
