9c41eaa1b3d2d2209d59c1b4b82499a3d3258fd2
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2020-02-06 22:51:24 -0800

    replace logout component

--- client/app/components/Logout/index.tsx
+++ client/app/components/Logout/index.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { useHistory } from 'react-router-dom';
-import { Typography, Icon, Row, Col } from 'antd';
-
-const { Paragraph, Text } = Typography;
+import { Result } from 'antd';
 
 const Logout = () => {
   const [seconds, setSeconds] = useState(4);
@@ -24,26 +22,11 @@ const Logout = () => {
   }, [seconds]);
 
   return (
-    <div className="logout">
-      <Row type="flex" align="middle">
-        <Col xs={3} md={2}>
-          <Icon
-            type="check-circle"
-            theme="outlined"
-            style={{ fontSize: '30px', color: '#08c' }}
-          />
-        </Col>
-        <Col xs={21} md={22}>
-          <Paragraph>
-            <Text strong>You have logged out successfully!</Text>
-          </Paragraph>
-          <Paragraph>
-            You will be redirected to the home page in{' '}
-            <span className="seconds">{seconds}</span> seconds
-          </Paragraph>
-        </Col>
-      </Row>
-    </div>
+    <Result
+      status="success"
+      title="You have logged out successfully!"
+      subTitle={`You will be redirected to the home page in ${seconds} seconds`}
+    />
   );
 };
 
